24978b96f7b1357892f77b3832671a101a235b35
Mike Barajas <mikebarajas@live.com>
2018-03-14 19:46:19 -0500

    asdf

--- austin_traffic/app.py
+++ austin_traffic/app.py
@@ -109,4 +109,4 @@ def pieChartData():
     return jsonify(dictionary)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=os.environ.get('PORT'))
